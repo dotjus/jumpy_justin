abc1f1ed5d3a9d2c35a7ca9d82ef0fad11fe7cb5
“Justin <dotjus@gmail.com>
2020-12-19 18:29:10 +1100

    Added game over aler if player character is hit by a block.

--- script.js
+++ script.js
@@ -7,5 +7,15 @@ function jump(){
             character.classList.remove("animate");
         },550);
     }
-
 }
+
+var checkDead  = setInterval(function(){
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"))
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>0 && characterTop>=130){
+            block.style.animation = "none"
+            block.style.display = "none"
+            alert("Oh noze!! You loose.")
+        }
+
+},10)
